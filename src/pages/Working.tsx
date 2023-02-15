d94a0731ba38b85b6df67a3eb19fb6935ec7f4f9
jaeuk Lee <lskkhj1005@naver.com>
2023-02-16 00:59:37 +0900

    feat: detail page

--- src/pages/Working.tsx
+++ src/pages/Working.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom';
+import { RootState } from '../redux/config/configStore';
 
 function Working() {
+
+  const params = useParams();
+
+  const data: any = useSelector((state: RootState) => {
+    return state.todo
+  })
+
+  console.log("data", data)
+
+  const datas = data.todo.find((item: any) => {
+    return String(item.id) === params.id;
+  })
+
   return (
-    <div>Working</div>
+    <div>
+      <div>
+        <span>ID: {datas.id}</span>
+
+        <Link to={'/'}><button>이전 페이지로</button></Link>
+
+        <h3>{datas.title}</h3>
+        <p>{datas.desc}</p>
+      </div>
+    </div>
   )
 }
 
