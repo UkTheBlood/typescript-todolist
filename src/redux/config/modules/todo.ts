2e1418f4a23fb36a2fc98eaf8c6fe49ae00a0e99
jaeuk Lee <lskkhj1005@naver.com>
2023-02-15 14:39:59 +0900

    feat: configStorefile, todo

--- src/redux/config/modules/todo.ts
+++ src/redux/config/modules/todo.ts
@@ -1,10 +1,69 @@
+import React from 'react'
 import { todos } from '../../../shared/todos'
 
-const addTodo = 'todo/add_todo'
+const add_Todo = 'todo/add_todo'
 const delete_Todo = 'todo/delete_Todo'
 const done_Todo = 'todo/done_Todo'
 const cancel_Todo = 'todo/cancel_Todo'
 
+interface todotype {
+    id: number;
+    title: string;
+    desc: string;
+    done: boolean;
+}
+
+export const addTodo = (payload: todotype) => {
+    return {
+        type: add_Todo,
+        payload: payload,
+    }
+}
+
+export const deleteTodo = (payload: todotype) => {
+    return {
+        type: delete_Todo,
+        payload,
+    }
+}
+
+export const doneTodo = (payload: todotype) => {
+    return {
+        type: done_Todo,
+        payload,
+    }
+}
+
+export const cancelTodo = (payload: todotype) => {
+    return {
+        type: cancel_Todo,
+        payload,
+    }
+}
+
 const initialState = {
     todo: todos,
 }
+
+const todo = (state = initialState, action: any) => {   // 질문하기
+    switch (action.type) {
+        case add_Todo:
+            return {
+                todo: [...state.todo, action.payload]
+            }
+        case delete_Todo:
+            return {
+                todo: action.payload
+            }
+        case done_Todo:
+            return {
+                todo: action.payload
+            }
+        case cancel_Todo:
+            return {
+                todo: action.payload
+            }
+    }
+}
+
+export default todo
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Working from '../pages/Working'
